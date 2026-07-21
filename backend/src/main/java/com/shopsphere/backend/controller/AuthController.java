b5ae59586e88e6f6038e218e64e7296f76c0bf2c
RAHUL2525252525 <108581617+RAHUL2525252525@users.noreply.github.com>
2026-07-21 13:08:35 +0530

    Update AuthController.java

--- backend/src/main/java/com/shopsphere/backend/controller/AuthController.java
+++ backend/src/main/java/com/shopsphere/backend/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://shopsphere-vnpx.vercel.app"})
 public class AuthController {
 
     @Autowired
