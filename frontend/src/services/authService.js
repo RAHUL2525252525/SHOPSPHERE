053f62ec618d60ca82b98b0aa97c6a4ce611e336
RAHUL2525252525 <108581617+RAHUL2525252525@users.noreply.github.com>
2026-07-22 09:32:59 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import API from "./api";
 
 export const loginUser = async (data) => {
-    const response = await API.post("/api/users/login", data);
+    const response = await API.post("/users/login", data);
     if (response.data) {
         localStorage.setItem("token", response.data.token);
         localStorage.setItem("role", response.data.role);
@@ -13,17 +13,17 @@ export const loginUser = async (data) => {
 };
 
 export const registerUser = async (data) => {
-    const response = await API.post("/api/users/register", data);
+    const response = await API.post("/users/register", data);
     return response.data;
 };
 
 export const forgotPassword = async (email) => {
-    const response = await API.post("/api/users/forgot-password", { email });
+    const response = await API.post("/users/forgot-password", { email });
     return response.data;
 };
 
 export const resetPassword = async (email, newPassword) => {
-    const response = await API.post("/api/users/reset-password", {
+    const response = await API.post("/users/reset-password", {
         email,
         newPassword,
     });
