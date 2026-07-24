facac407064e6d47e4146e4c19058da321f62e25
RAHUL2525252525 <108581617+RAHUL2525252525@users.noreply.github.com>
2026-07-24 12:35:04 +0530

    Update ProductRepository.java

--- backend/src/main/java/com/shopsphere/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/shopsphere/backend/repository/ProductRepository.java
@@ -5,14 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
-
+public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory(String category);
-
     List<Product> findByBrand(String brand);
-
     List<Product> findByNameContainingIgnoreCase(String keyword);
-
     List<Product> findByActiveTrue();
-
+    List<Product> findByIdIn(List<Long> ids);
 }
